f5df1ea8e9b6c90e3908302db5c3a2d87bb5e012
DLUGI <272549@student.pwr.edu.pl>
2024-04-15 20:40:24 +0200

    somethings wrong with dynamicArray

--- Heap.hpp
+++ Heap.hpp
@@ -1,21 +1,16 @@
 #pragma once
 
-struct node
-{
-    node();
-    node(int key, int value);
-    
-    int key_;
-    int value_;
-};
+#include "dynamicArray.hpp"
 
 
 class Heap{
 private:
+    // liczone od 1
     node* ptr_;
     unsigned size_;
     unsigned capacity_;
 
+    void increaseCapacity();
 public:
     Heap(unsigned capacity);
     ~Heap();
@@ -27,5 +22,6 @@ public:
     unsigned returnSize();
 
 
-    void maxHeapify();
+    void maxHeapify(unsigned i);
+    void buildMaxHeap();
 };
